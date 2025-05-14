cbaaa606d1f9a7bece764f67db2fc723c081a672
EduarJP <eduarjp2502@gmail.com>
2025-05-14 14:08:43 -0500

    Login con Django

---
+++ Usuarios/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from .models import Usuario
+
+# Create your views here.
+def Login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data['usuario']
+            password = form.cleaned_data['password']
+            try:
+                user = Usuario.objects.get(usuario=usuario, password=password)
+                return redirect('dashboard')
+            except Usuario.DoesNotExist:
+                error_message = "Datos inválidos"
+                return render(request, 'login.html', {'form': form, 'error_message': error_message})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def Logout_view(request):
+    logout(request)
+    return redirect ('login')
+
+def dashboard_view (request):
+    return render(request, 'dashboard.html')
